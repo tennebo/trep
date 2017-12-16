385c839167c80c45aca0e262900b95f6ad48fca9
Per Tennebø <ptennebo@numerix.com>
2017-12-16 20:49:10 +0000

    More test coverage

--- src/test/groovy/com/friggsoft/rfa/consumer/StreamingClientTest.groovy
+++ src/test/groovy/com/friggsoft/rfa/consumer/StreamingClientTest.groovy
@@ -9,6 +9,9 @@ import org.junit.Test
 import org.mockito.Mockito
 
 import com.reuters.rfa.common.Event
+import com.reuters.rfa.omm.OMMAttribInfo
+import com.reuters.rfa.omm.OMMData
+import com.reuters.rfa.omm.OMMIterable
 import com.reuters.rfa.omm.OMMMsg
 import com.reuters.rfa.omm.OMMTypes
 import com.reuters.rfa.session.omm.OMMItemEvent
@@ -57,6 +60,33 @@ class StreamingClientTest {
         StreamingClient client = new StreamingClient(sink)
 
         // This should result in no calls to the sink
+        client.processEvent(event)
+
+        verify(sink, times(0)).accept(Mockito.<Quote>any())
+    }
+
+    @Test
+    void testItemEventData() {
+        OMMMsg ommMsg = Mockito.mock(OMMMsg.class)
+        Mockito.when(ommMsg.getState()).thenReturn(null)
+        Mockito.when(ommMsg.getDataType()).thenReturn(OMMTypes.FIELD_LIST)
+        Mockito.when(ommMsg.has(OMMMsg.HAS_ATTRIB_INFO)).thenReturn(true)
+        Mockito.when(ommMsg.getAttribInfo()).thenReturn(Mockito.mock(OMMAttribInfo.class))
+
+        OMMData payload = Mockito.mock(OMMData.class, Mockito.withSettings().extraInterfaces(OMMIterable.class))
+        Mockito.when(ommMsg.getPayload()).thenReturn(payload)
+
+        Iterator iterator = Mockito.mock(Iterator.class)
+        Mockito.when(payload.iterator()).thenReturn(iterator)
+
+        OMMItemEvent event = Mockito.mock(OMMItemEvent.class)
+        Mockito.when(event.getType()).thenReturn(Event.OMM_ITEM_EVENT)
+        Mockito.when(event.getMsg()).thenReturn(ommMsg)
+
+        Consumer<Quote> sink = Mockito.mock(Consumer.class)
+        StreamingClient client = new StreamingClient(sink)
+
+        // This should result in one call to the sink
         client.processEvent(event)
 
         verify(sink, times(0)).accept(Mockito.<Quote>any())
