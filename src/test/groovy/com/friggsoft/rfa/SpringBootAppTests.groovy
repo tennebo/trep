c7b52dd55a7fa0bfa5361f340a24c307de58a4c1
Per Tennebø <ptennebo@numerix.com>
2017-12-16 10:08:27 +0000

    Document; untabify

--- src/test/groovy/com/friggsoft/rfa/SpringBootAppTests.groovy
+++ src/test/groovy/com/friggsoft/rfa/SpringBootAppTests.groovy
@@ -5,11 +5,14 @@ import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
+/**
+ * Smoke-test the Spring context.
+ */
 @RunWith(SpringRunner)
 @SpringBootTest
 class SpringBootAppTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
